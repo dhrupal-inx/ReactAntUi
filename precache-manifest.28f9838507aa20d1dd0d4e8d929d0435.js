5042d9b07e20e6000500e1e18d8baa5d9d8b5efd
Dhrupal suthar <dhrupal@inheritx.com>
2020-05-22 10:06:41 +0530

    Updates

---
+++ precache-manifest.28f9838507aa20d1dd0d4e8d929d0435.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda8fb61aa3aad8928404e93cde9727e",
+    "url": "/ReactAntUi/index.html"
+  },
+  {
+    "revision": "1cf1be241ad8bc08c617",
+    "url": "/ReactAntUi/static/css/2.bcd82df2.chunk.css"
+  },
+  {
+    "revision": "05cf5e8708f2f00e639d",
+    "url": "/ReactAntUi/static/css/main.e76960e4.chunk.css"
+  },
+  {
+    "revision": "1cf1be241ad8bc08c617",
+    "url": "/ReactAntUi/static/js/2.a543dc2d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactAntUi/static/js/2.a543dc2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05cf5e8708f2f00e639d",
+    "url": "/ReactAntUi/static/js/main.4c728133.chunk.js"
+  },
+  {
+    "revision": "a371ae1956d7ef484fb4",
+    "url": "/ReactAntUi/static/js/runtime-main.633924bd.js"
+  },
+  {
+    "revision": "03bd51f47f4377f6e7ea58b2664f96fd",
+    "url": "/ReactAntUi/static/media/1.03bd51f4.jpg"
+  },
+  {
+    "revision": "97a964b21e7566f8b5e8f0cbb872ea63",
+    "url": "/ReactAntUi/static/media/2.97a964b2.jpg"
+  },
+  {
+    "revision": "51eeb750eeab47e210036eee1c946b64",
+    "url": "/ReactAntUi/static/media/3.51eeb750.jpg"
+  },
+  {
+    "revision": "3e4766b1682a797e8d5521f83f9d8174",
+    "url": "/ReactAntUi/static/media/logo.3e4766b1.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactAntUi/static/media/logo.5d5d9eef.svg"
+  }
+]);
