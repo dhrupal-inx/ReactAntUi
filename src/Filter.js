35c172559e8ea8e1b9f4de9016a4a9be4e1317c3
Dhrupal suthar <dhrupal@inheritx.com>
2020-05-22 10:11:12 +0530

    done boat listing screen

---
+++ src/Filter.js
@@ -0,0 +1,122 @@
+import React , { useState }from 'react';
+import { Input, Collapse, Select, Rate, Radio, PageHeader, Slider,DatePicker, Drawer, Button } from 'antd';
+import {
+    UserOutlined,
+    EnvironmentOutlined, CalendarOutlined, DollarCircleOutlined,
+    StarOutlined, TeamOutlined, FilterOutlined
+} from '@ant-design/icons';
+
+const { Panel } = Collapse;
+const { RangePicker } = DatePicker;
+const { Option } = Select;
+
+
+function onAfterChange(value) {
+    console.log('onAfterChange: ', value);
+  }
+
+class Filter extends React.Component {
+    state = { visible: false };
+
+    showDrawer = () => {
+      this.setState({
+        visible: true,
+      });
+    };
+  
+    onClose = () => {
+      this.setState({
+        visible: false,
+      });
+    };
+
+    render() {
+        return (
+           
+            <div>
+
+        <Button type="primary" onClick={this.showDrawer} style={{marginBottom:"20px"}}><FilterOutlined /> Filter </Button>
+                <Drawer
+                    title="Advance Filter"
+                    placement="left"
+                    onClose={this.onClose}
+                    visible={this.state.visible}
+                    width={350}
+                >
+                    <Collapse
+                    style={{ marginBottom: '10px' }}
+                    expandIcon={({ isActive }) => <EnvironmentOutlined style={{ fontSize: '18px', color: '#222' }} />}>
+                    <Panel header="Location" key="1" style={{ fontSize: '16px', fontWeight: '500', color: '#222' }}>
+                        <div><Input placeholder="Search Location" size="large" /> </div>
+                    </Panel>
+                </Collapse>
+
+                <Collapse
+                    style={{ marginBottom: '10px' }}
+                    expandIcon={({ isActive }) => <CalendarOutlined style={{ fontSize: '18px', color: '#222' }} />}>
+                    <Panel header="Select Date" key="2" style={{ fontSize: '16px', fontWeight: '500', color: '#222' }}>
+                        <div><RangePicker size="large" /></div>
+                    </Panel>
+                </Collapse>
+
+                <Collapse
+                    style={{ marginBottom: '10px' }}
+                    expandIcon={({ isActive }) => <TeamOutlined style={{ fontSize: '18px', color: '#222' }} />}>
+                    <Panel header="Select Passenger" key="3" style={{ fontSize: '16px', fontWeight: '500', color: '#222' }}>
+                        <div><Select defaultValue="1" style={{ width: '100%' }} size="large">
+                            <Option value="1">1</Option>
+                            <Option value="2">2</Option>
+                            <Option value="3">3</Option>
+                            <Option value="4">4</Option>
+                        </Select></div>
+                    </Panel>
+
+                </Collapse>
+
+
+                <Collapse
+                    style={{ marginBottom: '10px' }}
+                    expandIcon={({ isActive }) => <UserOutlined style={{ fontSize: '18px', color: '#222' }} />}>
+                    <Panel header="Select Capton" key="4" style={{ fontSize: '16px', fontWeight: '500', color: '#222' }}>
+                        <div>
+                            <Radio.Group defaultValue="a" buttonStyle="solid">
+                                <Radio.Button value="a">With Capton</Radio.Button>
+                                <Radio.Button value="b">Without Capton</Radio.Button>
+                            </Radio.Group>
+                        </div>
+                    </Panel>
+                </Collapse>
+
+                <Collapse
+                    style={{ marginBottom: '10px' }}
+                    expandIcon={({ isActive }) => <StarOutlined style={{ fontSize: '18px', color: '#222' }} />}>
+
+                    <Panel header="Select Rating" key="5" style={{ fontSize: '16px', fontWeight: '500', color: '#222' }}>
+                        <div><Rate /></div>
+                    </Panel>
+                </Collapse>
+
+                <Collapse
+                    style={{ marginBottom: '10px' }}
+                    expandIcon={({ isActive }) => <DollarCircleOutlined style={{ fontSize: '18px', color: '#222' }} />}>
+                    <Panel header="Select Price" key="6" style={{ fontSize: '16px', fontWeight: '500', color: '#222' }}>
+                        <div>
+                            <Slider
+                                range
+                                step={1}
+                                defaultValue={[0, 100]}
+                                onAfterChange={onAfterChange}
+                            />
+                        </div>
+                    </Panel>
+                </Collapse>
+                </Drawer>
+                
+
+                
+            </div>
+        )
+    }
+}
+
+export default Filter;
---
+++ src/header.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { Layout, Menu, Input, Button, Modal, Form, Checkbox  } from 'antd';
+import {  LockOutlined, UserOutlined, SearchOutlined} from '@ant-design/icons';
+
+
+const { Header } = Layout;
+
+class HeaderMain extends React.Component {
+  state = { visible: false };
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+    render() {
+      
+       return (
+        <div>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%', padding: '0 25px' }}>
+        <div className="logo">
+          <img src={require('./assest/img/logo.svg')} />
+        </div>
+        <div className="search-box">
+          <Input placeholder="Search..." prefix={<SearchOutlined />} />
+        </div>
+        <Menu theme="dark" mode="horizontal" >
+          <Menu.Item key="1" >Join Now</Menu.Item>
+          <Menu.Item key="2" onClick={this.showModal}>Sign Up</Menu.Item>
+          <Menu.Item key="3">Be A Captain</Menu.Item>
+          <Menu.Item key="4">Help</Menu.Item>
+          <Menu.Item key="5"> <Button>List Your Boat</Button></Menu.Item>
+        </Menu>
+       
+      </Header>
+
+
+<Modal
+title="Join Us"
+visible={this.state.visible}
+// onOk={this.handleOk}
+onCancel={this.handleCancel}
+>
+<Form
+  name="normal_login"
+  className="login-form"
+  initialValues={{
+    remember: true,
+  }}
+ 
+>
+  <Form.Item
+    name="username"
+    rules={[
+      {
+        required: true,
+        message: 'Please input your Username!',
+      },
+    ]}
+  >
+    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+  </Form.Item>
+  <Form.Item
+    name="password"
+    rules={[
+      {
+        required: true,
+        message: 'Please input your Password!',
+      },
+    ]}
+  >
+    <Input
+      prefix={<LockOutlined className="site-form-item-icon" />}
+      type="password"
+      placeholder="Password"
+    />
+  </Form.Item>
+  <Form.Item>
+    <Form.Item name="remember" valuePropName="checked" noStyle>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <a className="login-form-forgot" href="">
+      Forgot password
+</a>
+  </Form.Item>
+
+  <Form.Item>
+    <Button type="primary" htmlType="submit" className="login-form-button">
+      Log in
+</Button>
+Or <a href="">register now!</a>
+  </Form.Item>
+</Form>
+</Modal>
+</div>
+       )
+    } 
+}
+
+export default HeaderMain;
